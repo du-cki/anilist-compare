ce4064ef2de09646c28f9f82bae458ac2bc1275a
du-cki <70286349+du-cki@users.noreply.github.com>
2026-09-26 03:09:07 +0400

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
@@ -18,4 +20,5 @@ export default defineConfig({
   },
 
   integrations: [react()],
+  adapter: vercel(),
 });
